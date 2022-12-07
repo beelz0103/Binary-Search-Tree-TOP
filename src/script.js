cda9e05de03ea82e5ef68c3b6a8533c67a041ec2
praveen <anabathula.praveen103@gmail.com>
2022-12-07 23:48:18 +0530

    Add insert function implementation

--- src/script.js
+++ src/script.js
@@ -1,4 +1,11 @@
 import Tree from "./tree.js";
 
-const newTree = new Tree([1, 2, 3, 4, 5]);
+const newTree = new Tree([1, 2]);
 newTree.buidlTree();
+newTree.insert(3);
+newTree.insert(4);
+newTree.insert(5);
+newTree.insert(6);
+newTree.insert(8);
+newTree.insert(7);
+newTree.prnt();
--- src/tree.js
+++ src/tree.js
@@ -7,10 +7,57 @@ export default class Tree {
     this.root = null;
   }
 
-  buidlTree() {
-    const half = Math.floor(this.array.length / 2);
-    this.root = new Node(this.array[half]);
-    this.root.left = new Node(this.array[half - 1]);
-    this.root.right = new Node(this.array[half + 1]);
+  buidlTree(arr = this.array) {
+    if (arr.length === 0) return;
+    const half = Math.floor(arr.length / 2);
+    const [leftSide, rightSide] = [
+      arr.slice(0, half),
+      arr.slice(half + 1, arr.length),
+    ];
+    this.root = new Node(
+      arr[half],
+      this.buidlTree(leftSide),
+      this.buidlTree(rightSide)
+    );
+
+    return this.root;
+  }
+
+  insert(value) {
+    let node = this.root;
+    const newNode = new Node(value);
+    while (true) {
+      if (node.left === null && node.data > value) {
+        node.left = newNode;
+        return;
+      }
+      if (node.right === null && node.data < value) {
+        node.right = newNode;
+        return;
+      }
+      if (node.data > value) {
+        node = node.left;
+      } else {
+        node = node.right;
+      }
+    }
+  }
+
+  returnAraay(arr = this.array) {
+    if (arr.length == 0) return;
+    const half = Math.floor(arr.length / 2);
+    const [leftSide, rightSide] = [
+      arr.slice(0, half),
+      arr.slice(half + 1, arr.length),
+    ];
+    console.log(leftSide);
+    console.log(rightSide);
+    console.log(arr[half]);
+    this.returnAraay(leftSide);
+    this.returnAraay(rightSide);
+  }
+
+  prnt() {
+    prettyPrint(this.root);
   }
 }
